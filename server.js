e4c9ba541600482e78f7f8d75da8c733aaa4aeae
Ryan Miura <miurary@os1.engr.oregonstate.edu>
2018-03-05 17:40:07 -0800

    hey whats up hello

--- server.js
+++ server.js
@@ -9,3 +9,33 @@ var port = process.env.PORT || 3000;
 
 var mongoURL = 'mongodb://cs290_miurary:cs290_miurary@classmongo.engr.oregonstate.edu:27017/cs290_miurary';
 var mongoConnection = null;
+
+app.engine('handlebars', handlebars({
+  defaultLayout: 'main',
+}));
+
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  res.status(200).render('login');
+  console.log("== Server status", res.statusCode);
+});
+
+app.use(express.static('public'));
+
+app.get('*', function (req, res) {
+  res.status(404).render('404');
+  console.log("== Server status", res.statusCode);
+});
+
+MongoClient.connect (mongoURL, function (err, connection) {
+
+  if (err)
+    throw err;
+  mongoConnection = connection;
+  app.listen(port, function () {
+    console.log("== Server listening on port:", port);
+});
+});
